f90a34ae0024d18ae2b7c5434b9e462f1a84347c
Illia Savinov <dresar2404@gmail.com>
2026-09-08 22:07:53 +0200

    chore: add products/urls.py to register a router in a products app

---
+++ apps/backend/products/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+
+
+router = routers.DefaultRouter()
+
+#router.register("")
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "products"
