92cedde95c0891951bf2532c94901aa60dfd1786
Illia Savinov <dresar2404@gmail.com>
2026-09-09 12:19:42 +0200

    chore: add api/users/ to the project urls

--- apps/backend/config/urls.py
+++ apps/backend/config/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("products/", include("products.urls", namespace="products")),
+    path('api/admin/', admin.site.urls),
+    path("api/products/", include("products.urls", namespace="products")),
+    path("api/users/", include("users.urls", namespace="users")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
