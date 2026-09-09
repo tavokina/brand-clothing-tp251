15688555c444fea813062b856329b53b8c1a8dd2
Skoulskiy <yaskevychilya@gmail.com>
2026-09-10 01:17:54 +0300

    fixed: added browser-router for routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store/store';
 import './app/styles/styles.scss';
@@ -28,34 +28,36 @@ import { AdminProductsPage } from './pages/AdminProductsPage';
 function App() {
   return (
     <Provider store={store}>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/catalog' element={<CatalogPage />} />
-        <Route path='/product/:id' element={<ProductDetailsPage />} />
-        <Route path='/cart' element={<CartPage />} />
-        <Route path='/checkout' element={<CheckoutPage />} />
-        <Route path='/confirmation' element={<OrderConfirmationPage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/forgotpassword' element={<PasswordForgotPage />} />
-        <Route path='/resetpassword' element={<PasswordResetPage />} />
-        <Route path='/signin' element={<SignInPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/catalog' element={<CatalogPage />} />
+          <Route path='/product/:id' element={<ProductDetailsPage />} />
+          <Route path='/cart' element={<CartPage />} />
+          <Route path='/checkout' element={<CheckoutPage />} />
+          <Route path='/confirmation' element={<OrderConfirmationPage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/forgotpassword' element={<PasswordForgotPage />} />
+          <Route path='/resetpassword' element={<PasswordResetPage />} />
+          <Route path='/signin' element={<SignInPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
 
-        <Route path="/account" element={<AccountLayout />}>
-          <Route index element={<AccountPersonalInformationPage />} />
-          <Route path="orders" element={<AccountOrdersPage />} />
-          <Route path="favorites" element={<AccountFavoritesPage />} />
-        </Route>
-    
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminProductsPage />} />
-          <Route path="products/new" element={<AdminProductFormPage />} />
-          <Route path="products/:id" element={<AdminProductFormPage />} />
-          <Route path="orders" element={<AdminOrdersPage />} />
-        </Route>
+          <Route path="/account" element={<AccountLayout />}>
+            <Route index element={<AccountPersonalInformationPage />} />
+            <Route path="orders" element={<AccountOrdersPage />} />
+            <Route path="favorites" element={<AccountFavoritesPage />} />
+          </Route>
+      
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminProductsPage />} />
+            <Route path="products/new" element={<AdminProductFormPage />} />
+            <Route path="products/:id" element={<AdminProductFormPage />} />
+            <Route path="orders" element={<AdminOrdersPage />} />
+          </Route>
 
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
