69ce7a254a8a09536ee53a41577b0c1aeec66bf0
Illia Savinov <dresar2404@gmail.com>
2026-09-09 18:13:56 +0200

    feat: add PasswordResetRequestSerializer and PasswordResetConfirmSerializer for password reset and confirm feature

--- apps/backend/users/serializers.py
+++ apps/backend/users/serializers.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.utils.encoding import force_str
+from users.utils import send_password_reset_link
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+
 
 User = get_user_model()
 
@@ -33,3 +40,55 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "email", "name", "age", "phone_number", "instagram"]
         read_only_fields = ['id', 'email']
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def save(self, **kwargs):
+        request = kwargs.get("request")
+        email = self.validated_data["email"]
+
+        try:
+            user = User.objects.get(email=email)
+
+        except User.DoesNotExist:
+            return
+
+        send_password_reset_link(request, user)
+
+
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+    new_password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate_new_password(self, value):
+        try:
+            validate_password(value)
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(list(e.message))
+        return value
+
+
+
+    def validate(self, attrs):
+        try:
+            uid = force_str(urlsafe_base64_decode(attrs['uid']))
+            user = User.objects.get(pk=uid)
+        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            raise serializers.ValidationError({"uid": "Invalid or expired link."})
+
+        if not default_token_generator.check_token(user, attrs['token']):
+            raise serializers.ValidationError({"token": "Invalid or expired token."})
+
+        attrs['user'] = user
+        return attrs
+
+    def save(self):
+        user = self.validated_data['user']
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
