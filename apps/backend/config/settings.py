f65042037bd8c06dc3f5ea7c4a8bd83fcb93d9e5
Illia Savinov <dresar2404@gmail.com>
2026-09-09 18:12:34 +0200

    chore: add more settings to simple_jwt. Refresh token is alive for 7 days and access token for 15 minutes. Add simplejwt.token_blacklist to installed apps for logout (logout requires refresh token from user for database to blacklist it and successfully logout

--- apps/backend/config/settings.py
+++ apps/backend/config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "products",
     "users",
@@ -111,7 +114,10 @@ DATABASES = {
 
 
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("Bearer",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 # Password validation
