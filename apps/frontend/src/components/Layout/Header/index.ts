65d172304b30f25b0fc49e10be9cf261005a4d0c
Skoulskiy <yaskevychilya@gmail.com>
2026-09-10 01:08:21 +0300

    fix: resolve merge conflict in Header index.ts

--- apps/frontend/src/components/Layout/Header/index.ts
+++ apps/frontend/src/components/Layout/Header/index.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD:apps/frontend/src/widgets/Header/index.ts
 export { Header } from './Header';
-=======
-export { MainPage } from './HomePage';
->>>>>>> develop:apps/frontend/src/components/Layout/Header/index.ts
