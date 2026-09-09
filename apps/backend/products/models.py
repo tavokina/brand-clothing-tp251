8aca3dccd93b1d45ef9035eb04b6abb5c307cabe
Illia Savinov <dresar2404@gmail.com>
2026-09-09 11:07:33 +0200

    fix: fixed product model according to the client's requirements (excel table)

--- apps/backend/products/models.py
+++ apps/backend/products/models.py
@@ -4,9 +4,18 @@ from django.db import models
 class Fabric(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.TextField()
+    type = models.ForeignKey(ProductType, on_delete=models.PROTECT, related_name="products")
     description = models.TextField(blank=True)
     discount_percent = models.PositiveSmallIntegerField(default=0)
     price_uah = models.DecimalField(max_digits=10, decimal_places=2)
@@ -19,13 +28,28 @@ class Product(models.Model):
             models.CheckConstraint(condition=models.Q(discount_percent__lte=100), name="product_discount_lte_100")
         ]
 
+    def __str__(self):
+        return f"{self.type} - {self.name}"
+
 
 class ProductFabric(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    fabric = models.ForeignKey(Fabric, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="fabrics")
+    fabric = models.ForeignKey(Fabric, on_delete=models.CASCADE, related_name="products")
     is_available = models.BooleanField(default=True) #is this fabric currently available for this product
 
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=["product", "fabric"], name="unique_product_fabric")
         ]
+
+    def __str__(self):
+        return f"{self.product.name} - {self.fabric.name}"
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="products/")
+    order = models.PositiveSmallIntegerField(default=0)
+
+
+    class Meta:
+        ordering = ["order"]
