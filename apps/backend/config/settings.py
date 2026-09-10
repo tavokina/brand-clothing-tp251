06eb533c47138b14a08976f7c45031c51710e1ba
Bohdan Romanov <bytovskyden@gmail.com>
2026-09-10 19:27:05 +0300

    chore: add orders and payments to INSTALLED_APPS

--- apps/backend/config/settings.py
+++ apps/backend/config/settings.py
@@ -46,8 +46,11 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
+    "drf_spectacular",
     "products",
     "users",
+    "orders",
+    "payments",
 ]
 
 MIDDLEWARE = [
