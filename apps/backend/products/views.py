917efa7bb489ec97e55b4b22d96e63e902d6c8ab
Illia Savinov <dresar2404@gmail.com>
2026-09-10 13:27:33 +0200

    feat: add ProductViewSet with basic serializers classes for List and Detail Views

--- apps/backend/products/views.py
+++ apps/backend/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from products.serializers import (ProductListSerializer,
+                                  ProductDetailSerializer)
+from products.models import Product
+
+
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProductListSerializer
+        return ProductDetailSerializer
