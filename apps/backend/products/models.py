6806e0b943fa71b33b6cc23af638788d8ba57faf
Illia Savinov <dresar2404@gmail.com>
2026-09-10 13:26:48 +0200

    fix: fixed Products model. Delete ProductFabric, add Color and ProductColor models. Add UniqueConstraint to ProductImage for order = 0 being unique

--- apps/backend/products/models.py
+++ apps/backend/products/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 
-class Fabric(models.Model):
+class Color(models.Model):
     name = models.CharField(max_length=100)
+    hex_code = models.CharField(max_length=7, blank=True) # for example "#FF0000" for frontend
+
 
     def __str__(self):
         return self.name
@@ -17,6 +19,7 @@ class Product(models.Model):
     name = models.TextField()
     type = models.ForeignKey(ProductType, on_delete=models.PROTECT, related_name="products")
     description = models.TextField(blank=True)
+    fabric_composition = models.CharField(max_length=255, blank=True) # "100% cotton"
     discount_percent = models.PositiveSmallIntegerField(default=0)
     price_uah = models.DecimalField(max_digits=10, decimal_places=2)
     price_usd = models.DecimalField(max_digits=10, decimal_places=2)
@@ -32,24 +35,24 @@ class Product(models.Model):
         return f"{self.type} - {self.name}"
 
 
-class ProductFabric(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="fabrics")
-    fabric = models.ForeignKey(Fabric, on_delete=models.CASCADE, related_name="products")
-    is_available = models.BooleanField(default=True) #is this fabric currently available for this product
+class ProductColor(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="colors")
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name="products")
+    is_available = models.BooleanField(default=True) # is this color available for this product right now
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["product", "fabric"], name="unique_product_fabric")
-        ]
-
-    def __str__(self):
-        return f"{self.product.name} - {self.fabric.name}"
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to="products/")
-    order = models.PositiveSmallIntegerField(default=0)
+    order = models.PositiveSmallIntegerField(default=0) #for frontend. Order = 0 decides which photo is main one
 
 
     class Meta:
         ordering = ["order"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["product"],
+                condition=models.Q(order=0),
+                name="unique_main_image_per_product"
+            )
+        ]
