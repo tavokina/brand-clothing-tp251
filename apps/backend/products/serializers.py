fffc07ab984a29a21d40ae11a402b5f5640329aa
Illia Savinov <dresar2404@gmail.com>
2026-09-11 13:39:28 +0200

    feat: Add Language and Currency Mixins. Fixed _get_discounted_price to return None if there is no discount on the product. Add get_description to SizeGuide. Add price and discounted_price to ProductListSerializer that use either uah/ua or usd/en language/currency depending on user settings. Same appeals to ProductDetailSeralizer which additionaly gets ProductType corresponding to user language settings

--- apps/backend/products/serializers.py
+++ apps/backend/products/serializers.py
@@ -1,25 +1,49 @@
 from rest_framework import serializers
 from decimal import Decimal, ROUND_HALF_UP
-from products.models import Product, ProductImage, Color, ProductColor, SizeGuide
+from products.models import (Product,
+                             ProductImage,
+                             Color,
+                             ProductColor,
+                             SizeGuide)
 
+
+class LanguageMixin:
+    def _lang(self):
+        request = self.context.get("request")
+        lang = request.GET.get("lang", "ua") if request else "ua"
+        return "eng" if lang == "en" else "ua"
+
+
+class CurrencyMixin:
+    def _currency(self):
+        request = self.context.get("request")
+        currency = request.GET.get("currency", "uah") if request else "uah"
+        return "usd" if currency == "usd" else "uah"
 
 def _calc_discounted(price, discount_percent):
     multiplier = Decimal(100 - discount_percent) / Decimal(100)
     discounted = price * multiplier
     return str(discounted.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))
 
-def _get_discounted_price_uah(obj):
-    return _calc_discounted(obj.price_uah, obj.discount_percent)
 
-def _get_discounted_price_usd(obj):
-    return _calc_discounted(obj.price_usd, obj.discount_percent)
+def _get_discounted_price(obj, currency):
+    if not obj.discount_percent:
+        return None
+    price = obj.price_usd if currency == "usd" else obj.price_uah
+    return _calc_discounted(price, obj.discount_percent)
 
 
-class SizeGuideSerializer(serializers.ModelSerializer):
-    product_type = serializers.SlugRelatedField(many=False, slug_field="name", read_only=True)
+
+class SizeGuideSerializer(LanguageMixin, serializers.ModelSerializer):
+    description = serializers.SerializerMethodField()
+
     class Meta:
         model = SizeGuide
-        fields = ("id", "product_type", "image", "description")
+        fields = ("id", "image", "description")
+
+    def get_description(self, obj):
+        return obj.description_eng if self._lang() == "eng" else obj.description_ua
+
 
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -37,24 +61,30 @@ class ProductColorSerializer(serializers.ModelSerializer):
         model = ProductColor
         fields = ("colors", "is_available")
 
-class ProductListSerializer(serializers.ModelSerializer):
+class ProductListSerializer(LanguageMixin, CurrencyMixin, serializers.ModelSerializer):
     main_image = serializers.SerializerMethodField()
-    discounted_price_uah = serializers.SerializerMethodField()
-    discounted_price_usd = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+    discounted_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = ("id",
                   "name",
                   "main_image",
-                  "price_uah",
-                  "price_usd",
+                  "price",
+                  "discounted_price",
                   "is_available",
-                  "discounted_price_uah",
-                  "discounted_price_usd",
                   "is_bestseller",
                   "is_new_collection",
                   )
+
+    def get_price(self, obj):
+        price = obj.price_usd if self._currency() == "usd" else obj.price_uah
+        return str(price)
+
+    def get_discounted_price(self, obj):
+        return _get_discounted_price(obj, self._currency())
+
 
     def get_main_image(self, obj):
         first_image = obj.images.first() #we have ordering so it's gonna be an image with order = 0
@@ -62,36 +92,36 @@ class ProductListSerializer(serializers.ModelSerializer):
             return ProductImageSerializer(first_image, context=self.context).data
         return None
 
-    def get_discounted_price_uah(self, obj):
-        return _get_discounted_price_uah(obj)
 
-    def get_discounted_price_usd(self, obj):
-        return _get_discounted_price_usd(obj)
-
-
-class ProductDetailSerializer(serializers.ModelSerializer):
+class ProductDetailSerializer(LanguageMixin, CurrencyMixin, serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
     available_colors = ProductColorSerializer(source="colors", many=True)
-    type = serializers.SlugRelatedField(many=False, slug_field="name", read_only=True)
+    type = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
+    fabric_composition = serializers.SerializerMethodField()
     size_guide = serializers.SerializerMethodField()
-    discounted_price_uah = serializers.SerializerMethodField()
-    discounted_price_usd = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+    discounted_price = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ("id",
-                  "name",
-                  "type",
-                  "description",
-                  "price_uah",
-                  "price_usd",
-                  "discounted_price_uah",
-                  "discounted_price_usd",
-                  "is_bestseller",
-                  "images",
-                  "available_colors",
-                  "is_available",
-                  "size_guide")
+        fields = ("id", "name", "type", "description", "fabric_composition",
+                  "price", "discounted_price",
+                  "is_bestseller", "images", "available_colors", "is_available", "size_guide")
 
+    def get_price(self, obj):
+        price = obj.price_usd if self._currency() == "usd" else obj.price_uah
+
+        return str(price)
+
+    def get_type(self, obj):
+        return obj.type.name_eng if self._lang() == "eng" else obj.type.name_ua
+
+    def get_description(self, obj):
+        return obj.description_eng if self._lang() == "eng" else obj.description_ua
+
+    def get_fabric_composition(self, obj):
+        return obj.fabric_composition_eng if self._lang() == "eng" else obj.fabric_composition_ua
 
     def get_size_guide(self, obj):
         size_guide = getattr(obj.type, "size_guide", None)
@@ -99,8 +129,5 @@ class ProductDetailSerializer(serializers.ModelSerializer):
             return SizeGuideSerializer(size_guide, context=self.context).data
         return None
 
-    def get_discounted_price_uah(self, obj):
-        return _get_discounted_price_uah(obj)
-
-    def get_discounted_price_usd(self, obj):
-        return _get_discounted_price_usd(obj)
+    def get_discounted_price(self, obj):
+        return _get_discounted_price(obj, self._currency())
