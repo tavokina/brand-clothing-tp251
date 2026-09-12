6767f92277e8ff6d21683891daa3b39399575d38
Illia Savinov <dresar2404@gmail.com>
2026-09-12 22:32:41 +0200

    feat: add collections router to the project

--- apps/backend/products/urls.py
+++ apps/backend/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from products.views import ProductViewSet
+from products.views import ProductViewSet, CollectionViewSet
 
 router = routers.DefaultRouter()
 
+router.register("collections", CollectionViewSet, basename="collection")
 router.register("", ProductViewSet)
 
 urlpatterns = [path("", include(router.urls))]
